970b09dd19557b5eb25e2bee05196219a2b072e4
Titux Metal (DEV) <github@lgdweb.fr>
2021-11-04 17:43:37 +0100

    Adding routing.js file in config; adds routing path constants

---
+++ src/config/routing.js
@@ -0,0 +1,20 @@
+export const dashboardPath = {
+  home: '/dashboard',
+  education: '/dashboard/education',
+  experience: '/dashboard/experience',
+  profile: '/dashboard/profile'
+}
+
+export const mainPath = {
+  home: '/',
+  auth: '/auth'
+}
+
+export const postsPath = {
+  all: '/posts',
+  single: '/posts/:slug'
+}
+export const profilesPath = {
+  all: '/profiles',
+  single: '/profiles/:slug'
+}
