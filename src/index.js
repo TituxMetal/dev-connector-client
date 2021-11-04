7098f3b1944c49a20b0dc7f1c5c7cdaa814b39ad
Titux Metal (DEV) <github@lgdweb.fr>
2021-11-04 17:56:58 +0100

    Updating index.js file; uses the fake auth store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { render } from 'react-dom'
 
 import { MainStyles } from '~/core'
 import Routes from '~/routes'
+import { AuthProvider } from '~/store'
 
 const element = document.getElementById('root')
 
 render(
-  <MainStyles>
-    <Routes />
-  </MainStyles>,
+  <AuthProvider>
+    <MainStyles>
+      <Routes />
+    </MainStyles>
+  </AuthProvider>,
   element
 )
